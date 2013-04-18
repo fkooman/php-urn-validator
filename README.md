d69c06c76787fe94549b6b0c08948261c1a3027d
François Kooman <fkooman@tuxed.net>
2013-04-18 15:44:00 +0200

    fix README and update some little things

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This project includes an URN validator according to RFC 2141.
 # API
 Use the static function to validate the URN:
 
-    if (FALSE === \Validate\Urn::validate("urn:foo:bar")) {
+    require_once '/PATH/TO/php-urn-validator/lib/Validator/Urn.php';
+
+    if (FALSE === \Validator\Urn::validate("urn:foo:bar")) {
         // invalid URN
     } else {
         // valid URN
--- lib/Validator/Urn.php
+++ lib/Validator/Urn.php
@@ -20,17 +20,17 @@ namespace Validator;
 
 class Urn
 {
-    const REGEXP = '/^urn:[a-z0-9][a-z0-9-]{1,31}:([a-z0-9()+,-.:=@;$_!*\']|%(0[1-9a-f]|[1-9a-f][0-9a-f]))+$/i';
+    const URN_REGEXP = '/^urn:[a-z0-9][a-z0-9-]{1,31}:([a-z0-9()+,-.:=@;$_!*\']|%(0[1-9a-f]|[1-9a-f][0-9a-f]))+$/i';
 
     /**
-     * Validate a URN according to RFC 2141
+     * Validate a URN according to RFC 2141.
      *
      * @param urn the URN to validate
      * @return TRUE when the URN is valid, FALSE when invalid
      */
     public static function validate($urn)
     {
-        return (bool) preg_match(self::REGEXP, $urn);
+        return (bool) preg_match(self::URN_REGEXP, $urn);
     }
 
 }
