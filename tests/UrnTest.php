43530106bbfac335427751675287b5399d5dd218
François Kooman <fkooman@tuxed.net>
2013-04-18 17:44:12 +0200

    add some more test cases, add RFC2141 document to docs directory

--- tests/UrnTest.php
+++ tests/UrnTest.php
@@ -44,7 +44,21 @@ class UrnTest extends PHPUnit_Framework_TestCase
             array("urn:mace:dir:attribute-def:uid"),
             array("urn:foo:bar"),
             array("urn:oid:0.9.2342.19200300.100.1.3"),
-            array("urn:foo:%ff")
+            array("urn:foo:%ff"),
+            array("urn:x-:foo"),        // valid, but should it be?
+            array("urn:foo:bar:"),      // valid, but should it be?
+            array("urn:foo:bar::::::"), // valid, but should it be?
+
+            // from https://en.wikipedia.org/wiki/Uniform_resource_name#Examples
+            array("urn:isbn:0451450523"),
+            array("urn:isan:0000-0000-9E59-0000-O-0000-0000-2"),
+            array("urn:issn:0167-6423"),
+            array("urn:ietf:rfc:2648"),
+            array("urn:mpeg:mpeg7:schema:2001"),
+            array("urn:oid:2.16.840"),
+            array("urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66"),
+            array("urn:nbn:de:bvb:19-146642"),
+            array("urn:lex:eu:council:directive:2010-03-09;2010-19-UE"),
         );
     }
 
@@ -54,7 +68,10 @@ class UrnTest extends PHPUnit_Framework_TestCase
             array("urn:nl.surfconext.licenseInfo"),
             array("foo:bar:baz"),
             array("urn:f:bar"),
-            array("urn:foo:%00"),   // cannot contain %00
+            array("urn:foo:%00"),       // cannot contain %00
+            array("urn:x:foo"),         // NID should be at least length 2
+            array("urn:x-oauth:%gg"),   // encoding should be HEX
+            array("urn:%aa:xyz"),       // NID cannot contain encoded chars
         );
     }
 
